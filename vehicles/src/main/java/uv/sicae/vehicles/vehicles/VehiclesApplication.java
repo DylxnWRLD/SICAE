d153c866ac5fb3e013c97ed2e4ee4db7d448d68a
jeshuayam <jeshuayam3024@gmail.com>
2026-06-16 03:51:54 -0600

    Documentación del main

--- vehicles/src/main/java/uv/sicae/vehicles/vehicles/VehiclesApplication.java
+++ vehicles/src/main/java/uv/sicae/vehicles/vehicles/VehiclesApplication.java
@@ -3,6 +3,11 @@ package uv.sicae.vehicles.vehicles;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Main de la aplicación
+ * @author jeshu
+ */
+
 @SpringBootApplication
 public class VehiclesApplication {
 
